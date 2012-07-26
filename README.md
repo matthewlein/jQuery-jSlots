a6031ec3147020379a201c402e02fd6079fd714d
matthewlein <matthewlein@hotmail.com>
2012-07-26 15:23:25 -0400

    added array winnerNumber

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ These are the options, with their default values, and what they do
 
     $.jSlots.defaultOptions = {
         number : 3,          // Number: number of slots
-        winnerNumber : 1,    // Number: list item number upon which to trigger a win, 1-based index, NOT ZERO-BASED
+        winnerNumber : 1,    // Number or Array: list item number(s) upon which to trigger a win, 1-based index, NOT ZERO-BASED
         spinner : '',        // CSS Selector: element to bind the start event to
         spinEvent : 'click', // String: event to start slots on this event
         onStart : $.noop,    // Function: runs on spin start,
@@ -18,21 +18,21 @@ These are the options, with their default values, and what they do
         time : 7000,         // Number: total time of spin animation
         loops : 6            // Number: times it will spin during the animation
     };
-    
-## Usage 
+
+## Usage
 
 Attach jQuery (successfully tested down to v1.4.1)
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-    
+
 Attach jSlots plugin
 
     <script src="jquery.jSlots.js" charset="utf-8"></script>
-    
+
 Attach easing plugin (optional but HIGHLY recommended for nice animation)
 
     <script src="jquery.easing.1.3.js" charset="utf-8"></script>
-    
+
 Create a list and an element that will spin the slots
 
     <ul class="slot">
@@ -44,21 +44,21 @@ Create a list and an element that will spin the slots
         <li>6</li>
         <li>7</li>
     </ul>
-    
+
     <!-- this button will start the spin -->
     <input type="button" id="playBtn" value="play">
 
 Target the list and make it a jSlot!
-    
+
     <script type="text/javascript" charset="utf-8">
-        
+
         $('.slot').jSlots({
             spinner : '#playBtn',
             winnerNumber : 7
         });
-        
+
     </script>
-    
+
 Styling is up to you, but jSlots supplies a jSlots-wrapper div around your lists that should get `overflow: hidden` and a height set on it. Here are some recommended styles:
 
     .jSlots-wrapper {
--- jquery.jSlots.js
+++ jquery.jSlots.js
@@ -2,39 +2,39 @@
  * jQuery jSlots Plugin
  * http://matthewlein.com/jslot/
  * Copyright (c) 2011 Matthew Lein
- * Version: 1.0.1 (2/08/2012)
+ * Version: 1.0.2 (7/26/2012)
  * Dual licensed under the MIT and GPL licenses
  * Requires: jQuery v1.4.1 or later
  */
 
 (function($){
-    
+
     $.jSlots = function(el, options){
 
         var base = this;
-        
+
         base.$el = $(el);
         base.el = el;
-        
+
         base.$el.data("jSlots", base);
-        
+
         base.init = function() {
-            
+
             base.options = $.extend({},$.jSlots.defaultOptions, options);
-            
+
             base.setup();
             base.bindEvents();
-            
+
         };
-        
-        
+
+
         // --------------------------------------------------------------------- //
         // DEFAULT OPTIONS
         // --------------------------------------------------------------------- //
 
         $.jSlots.defaultOptions = {
             number : 3,          // Number: number of slots
-            winnerNumber : 1,    // Number: list item number upon which to trigger a win, 1-based index, NOT ZERO-BASED
+            winnerNumber : 1,    // Number or Array: list item number(s) upon which to trigger a win, 1-based index, NOT ZERO-BASED
             spinner : '',        // CSS Selector: element to bind the start event to
             spinEvent : 'click', // String: event to start slots on this event
             onStart : $.noop,    // Function: runs on spin start,
@@ -44,67 +44,67 @@
             time : 7000,         // Number: total time of spin animation
             loops : 6            // Number: times it will spin during the animation
         };
-        
+
         // --------------------------------------------------------------------- //
         // HELPERS
         // --------------------------------------------------------------------- //
-        
+
         base.randomRange = function(low, high) {
             return Math.floor( Math.random() * (1 + high - low) ) + low;
         };
-        
+
         // --------------------------------------------------------------------- //
         // VARS
         // --------------------------------------------------------------------- //
-        
+
         base.isSpinning = false;
         base.spinSpeed = 0;
         base.winCount = 0;
         base.doneCount = 0;
-        
+
         base.$liHeight = 0;
         base.$liWidth = 0;
-        
+
         base.winners = [];
         base.allSlots = [];
-        
+
         // --------------------------------------------------------------------- //
         // FUNCTIONS
         // --------------------------------------------------------------------- //
-        
-        
+
+
         base.setup = function() {
-            
+
             // set sizes
-            
+
             var $list = base.$el;
             var $li = $list.find('li').first();
-            
+
             base.$liHeight = $li.outerHeight();
             base.$liWidth = $li.outerWidth();
-            
+
             base.liCount = base.$el.children().length;
-            
+
             base.listHeight = base.$liHeight * base.liCount;
-            
+
             base.increment = (base.options.time / base.options.loops) / base.options.loops;
-                
+
             $list.css('position', 'relative');
-            
+
             $li.clone().appendTo($list);
-            
+
             base.$wrapper = $list.wrap('<div class="jSlots-wrapper"></div>').parent();
-            
+
             // remove original, so it can be recreated as a Slot
             base.$el.remove();
-            
+
             // clone lists
             for (var i = base.options.number - 1; i >= 0; i--){
                 base.allSlots.push( new base.Slot() );
             }
-            
+
         };
-        
+
         base.bindEvents = function() {
             $(base.options.spinner).bind(base.options.spinEvent, function(event) {
                 if (!base.isSpinning) {
@@ -112,26 +112,26 @@
                 }
             });
         };
-        
+
         // Slot contstructor
         base.Slot = function() {
-            
+
             this.spinSpeed = 0;
             this.el = base.$el.clone().appendTo(base.$wrapper)[0];
             this.$el = $(this.el);
             this.loopCount = 0;
             this.number = 0;
-            
+
         };
-        
-        
+
+
         base.Slot.prototype = {
-            
+
             // do one rotation
             spinEm : function() {
 
                 var that = this;
-                
+
                 that.$el
                     .css( 'top', -base.listHeight )
                     .animate( { 'top' : '0px' }, that.spinSpeed, 'linear', function() {
@@ -139,7 +139,7 @@
                     });
 
             },
-            
+
             lowerSpeed : function() {
 
                 this.spinSpeed += base.increment;
@@ -155,12 +155,12 @@
 
                 }
             },
-            
+
             // final rotation
             finish : function() {
-                
+
                 var that = this;
-                
+
                 var endNum = base.randomRange( 1, base.liCount );
 
                 var finalPos = - ( (base.$liHeight * endNum) - base.$liHeight );
@@ -171,49 +171,56 @@
                     .animate( {'top': finalPos}, finalSpeed, base.options.easing, function() {
                         base.checkWinner(endNum, that);
                     });
-                
+
             }
-            
+
         };
-        
+
         base.checkWinner = function(endNum, slot) {
-            
+
             base.doneCount++;
             // set the slot number to whatever it ended on
             slot.number = endNum;
 
-            if (endNum === base.options.winnerNumber) {
+            // if its in the winners array
+            if (
+                ( $.isArray( base.options.winnerNumber ) && base.options.winnerNumber.indexOf(endNum) > -1 ) ||
+                endNum === base.options.winnerNumber
+                ) {
+
+                // its a winner!
                 base.winCount++;
                 base.winners.push(slot.$el);
+
             }
 
             if (base.doneCount === base.options.number) {
-                
+
                 var finalNumbers = [];
-                
+
                 $.each(base.allSlots, function(index, val) {
                     finalNumbers[index] = val.number;
                 });
-                
+
                 if ( $.isFunction( base.options.onEnd ) ) {
                     base.options.onEnd(finalNumbers);
                 }
-                
+
                 if ( base.winCount && $.isFunction(base.options.onWin) ) {
                     base.options.onWin(base.winCount, base.winners, finalNumbers);
                 }
                 base.isSpinning = false;
             }
         };
-        
-        
+
+
         base.playSlots = function() {
 
             base.isSpinning = true;
             base.winCount = 0;
             base.doneCount = 0;
             base.winners = [];
-            
+
             if ( $.isFunction(base.options.onStart) ) {
                 base.options.onStart();
             }
@@ -223,26 +230,26 @@
                 this.loopCount = 0;
                 this.spinEm();
             });
-            
+
         };
 
-        
+
         base.onWin = function() {
             if ( $.isFunction(base.options.onWin) ) {
                 base.options.onWin();
             }
         };
-        
-        
+
+
         // Run initializer
         base.init();
     };
-    
-    
+
+
     // --------------------------------------------------------------------- //
     // JQUERY FN
     // --------------------------------------------------------------------- //
-    
+
     $.fn.jSlots = function(options){
         if (this.length) {
             return this.each(function(){
@@ -250,5 +257,5 @@
             });
         }
     };
-    
+
 })(jQuery);
